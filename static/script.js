8bf15f633ac047929a83a96469b4a97bda68f94b
Scott <smurr4@uwo.ca>
2022-11-19 16:39:18 -0500

    Created main page and directed to it

--- static/script.js
+++ static/script.js
@@ -6,15 +6,15 @@ function verifyExistingUsername() {
     fetch(url)
         .then(res => res.json()
             .then(data => {
-                if(data[0]){
+                if (data[0]) {
                     let place = document.getElementById('nextButton')
                     let details = document.createElement('button')
                     let link = document.createElement('a');
-                    link.href = "newUser.html"
+                    link.href = "mainPage.html"
                     link.appendChild(document.createTextNode('Next'))
                     details.appendChild(link)
                     place.appendChild(details);
-                }else{
+                } else {
                     //code for invalid username
                 }
             }))
@@ -29,10 +29,10 @@ function verifyNewUsername() {
         .then(res => res.json()
             .then(data => {
                 console.log(data)
-                if(data.length == 0){
+                if (data.length == 0) {
                     // create entered username and add to DB 
                     addUsernames();
-                }else{
+                } else {
                     //code for invalid username
                     document.getElementById('addUser-status').innerText = "This username is taken! Please try again."
 
@@ -40,19 +40,27 @@ function verifyNewUsername() {
             }))
 }
 
-function addUsernames(){
+function addUsernames() {
     let path = '/add';
 
     fetch(path, {
         method: "POST",
-        headers: { 'Content-type': "application/json"},
+        headers: { 'Content-type': "application/json" },
         body: JSON.stringify({
             username: document.getElementById("newUsername").value
         })
     })
-    .then(res => {
-        document.getElementById('addUser-status').innerText = "Your username was successfully created!!"
-    })
+        .then(res => {
+            document.getElementById('addUser-status').innerText = "Your username was successfully created!!"
+
+            let place = document.getElementById('addUser-status')
+            let details = document.createElement('button')
+            let link = document.createElement('a');
+            link.href = "mainPage.html"
+            link.appendChild(document.createTextNode('Next'))
+            details.appendChild(link)
+            place.appendChild(details);
+        })
 }
 
 
